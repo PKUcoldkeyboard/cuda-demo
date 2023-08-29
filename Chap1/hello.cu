4f0851506ad6e5c86d6d7a2d2d0f977b88b257a3
cuterwrite <158505862@qq.com>
2023-08-29 19:47:56 +0800

    update Dir Name

--- Chap1/hello.cu
+++ Chap1/hello.cu
@@ -2,20 +2,23 @@
 #include <stdlib.h>
 #include "dbg.h"
 
-__global__ void helloFromGPU(void) {
+__global__ void helloFromGPU(void)
+{
     int threadId = threadIdx.x;
     printf("Hello from GPU! threadId: %d\n", threadId);
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     cudaError_t cudaStatus = cudaSuccess;
     printf("Hello from CPU!\n");
 
     int blockDim = 10;
-    int gridDim = 1;
+    int gridDim  = 1;
     helloFromGPU<<<gridDim, blockDim>>>();
     cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
+    if (cudaStatus != cudaSuccess)
+    {
         log_err("Failed to synchronize, %s\n", cudaGetErrorString(cudaStatus));
         cudaDeviceReset();
         return -1;
